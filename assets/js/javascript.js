3e652d9a1b6caf7add59d6b73fb6bdba20a625fe
feridzeyn <feridzeynalov12@gmail.com>
2024-09-12 22:34:28 +0200

    Added X icon to menu

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,14 @@
-let icon = document.querySelector("#bar_icon");
+let bar_icon = document.querySelector("#bar_icon");
+let close_icon = document.querySelector("#close_icon")
 let animation = document.querySelector(".navAnimation");
 let img = document.querySelector("#logo")
-icon.addEventListener("click", ()=>{
-animation.classList.toggle("active")
-document.querySelector(".first-body").classList.toggle("mystyle")
-img.classList.toggle("mystyle1")
+
+bar_icon.addEventListener("click", ()=>{
+animation.classList.add("active")
+document.body.classList.add("mystyle")
+})
+
+close_icon.addEventListener("click", ()=>{
+animation.classList.remove("active")
+document.body.classList.remove("mystyle")
 })
