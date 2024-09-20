1721144d98574b996199fe6d385d6ab16bb88687
feridzeyn <feridzeynalov12@gmail.com>
2024-09-20 16:14:34 +0200

    added slider

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -12,3 +12,22 @@ close_icon.addEventListener("click", ()=>{
 animation.classList.remove("active")
 document.body.classList.remove("mystyle")
 })
+
+// $(document).ready(function(){
+//   $('.fifth-name').slick({
+//     infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+//   });
+// });
+
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  centeredSlides: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
