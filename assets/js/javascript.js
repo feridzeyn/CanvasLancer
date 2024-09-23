ab2f83fd3c6e872d1ae34d6bdb1f070e7343857a
feridzeyn <feridzeynalov12@gmail.com>
2024-09-23 14:16:10 +0200

    updated responsive and slider

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -23,11 +23,30 @@ document.body.classList.remove("mystyle")
 
 let swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
-  centeredSlides: true,
+  centeredSlides: false,
   spaceBetween: 30,
   grabCursor: true,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    "0": {
+        "slidesPerView": 2,
+        "slidesPerGroup": 1
+    },
+    "880": {
+        "slidesPerView": 3,
+        "slidesPerGroup": 1
+    },
+    "1025": {
+        "slidesPerView": 3,
+        "slidesPerGroup": 1
+    },
+    "1366": {
+        "slidesPerView": 4,
+        "slidesPerGroup": 1
+    }
+  }
 });
